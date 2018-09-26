ff7d7182f6d07b108a173b13eebb27f2768840f6
Connor Couetil <couetilc@gmail.com>
2018-09-26 12:21:26 -0400

    added responsive styling to consulting and balf pages

--- client/react-app/src/App.js
+++ client/react-app/src/App.js
@@ -10,6 +10,7 @@ import logoBig from './assets/logo-breath-main-680w.png';
 import homeIcon from './assets/home_white_18x18.png';
 import imgBalf from './assets/bal-procedure.png';
 import imgCytology from './assets/bal-cytology.png';
+import imgAero from './assets/llc-aerohippus.png';
 import picLaurent from './assets/picture-laurent-purple-ltr.png';
 import picKathleen from './assets/picture-kathleen-orange-rtl.png';
 
@@ -57,9 +58,11 @@ class BalfSample extends Component {
         <div id="balf-sample-page" className="blank-page">
 
             <div id="balf-tagline" className="tagline">
-                <h1>BAL Cytology Services</h1>
-                <h5>Equine Lung Experts is pleased to offer bronchoalveolar lavage (BAL) cytology services to veterinarians.  We process bronchoalveolar lavage fluid (BALF) samples and provide cytology interpretation to diagnose respiratory diseases in your equine patients.</h5>
-                <a href="/#people-summary">Meet the experts →</a>
+                <div className="tagline-content">
+                    <h1>BAL Cytology Services</h1>
+                    <h5>Equine Lung Experts is pleased to offer bronchoalveolar lavage (BAL) cytology services to veterinarians.  We process bronchoalveolar lavage fluid (BALF) samples and provide cytology interpretation to diagnose respiratory diseases in your equine patients.</h5>
+                    <a href="/#people-summary">Meet the experts →</a>
+                </div>
 
                 <div className="table-of-contents">
                     <span>Key Information</span>
@@ -140,26 +143,52 @@ class EnvironmentalConsulting extends Component {
         return (
         <div id="env-consulting-page" className="blank-page">
             <div id="env-tagline" className="tagline">
-                <h1>Environmental Assessment Services</h1>
-                <h5>Equine Lung Experts is pleased to offer environmental assessment services to veterinarians. We work with veterinarians to objectively assess barn and stall environments, and analyze potential sources of irritant or allergen exposure in horses.</h5>
-            
-                <a href="/#people-summary">Meet the experts →</a>
+                <div className="tagline-content">
+                    <h1>Environmental Assessment Services</h1>
+                    <h5>Equine Lung Experts is pleased to offer environmental assessment services to veterinarians. We work with veterinarians to objectively assess barn and stall environments, and analyze potential sources of irritant or allergen exposure in horses.</h5>
+                    <a href="/#people-summary">Meet the experts →</a>
+                </div>
 
                 <div className="table-of-contents">
                     <span>Key Information</span>
                     <a href="#env-what-we-do">What We Do</a>
-                    <a href="#env-request-service">Request Services</a>
+                    <a href="#env-request-services">Request Services</a>
                     <a href="#env-cost-service">Pricing</a>
                 </div>
             </div>
 
-            <div id="env-what-we-do">
-                <h3>What We Do</h3>
-                <ul>
-                    <li>We sample and quantify airborne irritants and allergens using gravimetric air sampling techniques and real time particulate counters.</li>
-                    <li>We assess dust and allergen sources by testing feedstuff and bedding samples.</li>
-                </ul>
-                <span>The service is tailored to each situation and the goals expressed by the horse’s owner and veterinarian.</span>
+            <div className="inline-image">
+                <div>
+                    <div id="env-what-we-do">
+                        <h3>What We Do</h3>
+                        <ul>
+                            <li>We sample and quantify airborne irritants and allergens using gravimetric air sampling techniques and real time particulate counters.</li>
+                            <li>We assess dust and allergen sources by testing feedstuff and bedding samples.</li>
+                        </ul>
+                        <span>The service is tailored to each situation and the goals expressed by the horse’s owner and veterinarian.</span>
+                    </div>
+
+                    <div id="env-request-services">
+                        <h3>How to Request Counseling Services</h3>
+                        <div>Please contact Equine Lung Experts by email (equinelungexperts@gmail.com) to discuss the type of service desired.  We will respond to the email within 1-2 working days.</div>
+                    </div>
+                </div>
+
+                <figure id="env-image-aero" className="figure-with-caption">
+                    <img src={imgAero} />
+                    <figcaption>Dr. Couetil delivering medication to a patient.</figcaption>
+                </figure>
+            </div>
+
+            <div id="env-cost-service">
+                <h3>Cost of Services</h3>
+                <div>The fee for environmental counseling services is based on the type of assistance needed. A quote will be sent following an inquiry and discussion of services requested.</div>
+            <ul>
+                <li>Fee is payable at the time of service.</li>
+                <li>We accept checks made payable to “Equine Lung Experts, LLC”.</li>
+                <li>Invoices not paid within thirty (30) days of the invoice date shall be charged a late fee equal to 1.5 % per month of the total unpaid balance.</li>
+            </ul>
+
             </div>
 
             
