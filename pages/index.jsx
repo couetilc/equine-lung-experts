4f8c756d7fabde8c0a9751ae9997f8727c2c9e31
Connor Couetil <couetilc@gmail.com>
2021-02-25 13:05:59 -0800

    removing class components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
 import ContactForm from '../components/ContactForm';
+import Link from '../components/Link';
 
 const picLaurent = '/picture-laurent-purple-ltr.jpg';
 const picKathleen = '/picture-kathleen-orange-rtl.jpg';
@@ -13,214 +13,143 @@ export default function Home () {
   );
 }
 
-class HomeBanner extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      default: {
-        titleA: 'BREATHE EASIER',
-        titleB: 'MOVE FASTER',
-        content:
-          'Respiratory diseases cause coughing and poor performance in racing, sport and recreational horses. Through diagnostic services, the veterinary scientists at Equine Lung Experts will help you diagnose and treat respiratory diseases caused by infections or environmental exposures.',
-        action: {
-          link: '#tagline-main',
-          text: 'LEARN MORE',
-        },
-      },
-    };
-  }
-
-  render() {
-    return <Banner {...this.state.default} />;
-  }
+function HomeBanner () {
+  return (
+    <section id="home-banner" className="banner">
+      <div id="home-banner-message" className="banner-message">
+        <h1>
+          <span className="title-a">
+            BREATHE EASIER
+          </span>
+          <span className="title-b">
+            MOVE FASTER
+          </span>
+        </h1>
+        <p>
+          Respiratory diseases cause coughing and poor performance in racing,
+          sport and recreational horses. Through diagnostic services, the
+          veterinary scientists at Equine Lung Experts will help you diagnose
+          and treat respiratory diseases caused by infections or
+          environmental exposures.
+        </p>
+        <a href="#tagline-main">
+          LEARN MORE
+        </a>
+      </div>
+    </section>
+  );
 }
 
-class HomeContent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      default: {
-        tagline: {
-          title: 'We solve respiratory problems.',
-          content:
-            "Veterinary diagnostic laboratories don't have the specialized knowledge in equine pulmonology to accurately diagnose respiratory diseases based on bronchoalveolar lavage fluid cytology samples. As leading specialists in horse asthma (heaves, RAO, IAD, COPD, and chronic bronchitis), exercise-induced pulmonary hemorrhage (EIPH or epistaxis), and other respiratory diseases affecting performance, we provide in-depth evaluations of pulmonary health and risk factors.",
-        },
-      },
-    };
-  }
-
-  render() {
-    return (
-      <main className="blank-page">
-        <Tagline {...this.state.default.tagline} />
-        <ServiceSummary />
-        <PeopleSummary />
-        <ContactForm />
-      </main>
-    );
-  }
+function HomeContent () {
+  return (
+    <main className="blank-page">
+      <Tagline title="We solve respiratory problems.">
+        Veterinary diagnostic laboratories don&apos;t have the specialized
+        knowledge in equine pulmonology to accurately diagnose respiratory
+        diseases based on bronchoalveolar lavage fluid cytology samples. As
+        leading specialists in horse asthma (heaves, RAO, IAD, COPD, and
+        chronic bronchitis), exercise-induced pulmonary hemorrhage (EIPH or
+        epistaxis), and other respiratory diseases affecting performance, we
+        provide in-depth evaluations of pulmonary health and risk factors.
+      </Tagline>
+      <ServiceSummary />
+      <PeopleSummary />
+      <ContactForm />
+    </main>
+  );
 }
 
-class Tagline extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...props };
-  }
-
-  render() {
-    return (
-      <section className="tagline">
-        <div id="tagline-main" className="tagline-content">
-          <h1>{this.state.title}</h1>
-          <h5>{this.state.content}</h5>
-        </div>
-      </section>
-    );
-  }
+function Tagline ({ title, children }) {
+  return (
+    <section className="tagline">
+      <div id="tagline-main" className="tagline-content">
+        <h1>{title}</h1>
+        <h5>{children}</h5>
+      </div>
+    </section>
+  );
 }
 
-class ServiceSummary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      default: {
-        summaryBalf: {
-          title: 'BALF Interpretation and  Diagnostics',
-          content:
-            'Submit a sample and we will return a report describing the microscopic appearance of cytology including cellularity, differential cell count, mucus characteristics, and overall cytologic interpretation. When appropriate, comments regarding etiology will be provided.',
-          action: {
-            link: '/balfsample',
-            text: 'Submit Sample',
-          },
-        },
-        summaryEval: {
-          title: 'Environmental Exposure Counseling',
-          content:
-            "Our experts can examine your horse's environment, measure stall or barn ventilation, and sample airborn irritants to identify potential causes of equine asthma and recommend appropriate interventions and treatments.",
-          action: {
-            link: '/environmentalconsulting',
-            text: 'Schedule Evaluation',
-          },
-        },
-      },
-    };
-  }
-
-  render() {
-    return (
-      <section id="service-summary">
-        <ServiceItem {...this.state.default.summaryBalf} />
-        <ServiceItem {...this.state.default.summaryEval} />
-      </section>
-    );
-  }
+function ServiceSummary () {
+  return (
+    <section id="service-summary">
+      <div className="service-item">
+        <h3>
+          BALF Interpretation and  Diagnostics
+        </h3>
+        <Link className="service-link" href="/balfsample">
+          Submit Sample
+        </Link>
+        <p>
+          Submit a sample and we will return a report describing the
+          microscopic appearance of cytology including cellularity,
+          differential cell count, mucus characteristics, and overall
+          cytologic interpretation. When appropriate, comments regarding
+          etiology will be provided.
+        </p>
+      </div>
+      <div className="service-item">
+        <h3>
+          Environmental Exposure Counseling
+        </h3>
+        <Link className="service-link" href="/environmentalconsulting">
+          Schedule Evaluation
+        </Link>
+        <p>
+          Our experts can examine your horse&apos;s environment, measure stall or
+          barn ventilation, and sample airborn irritants to identify
+          potential causes of equine asthma and recommend appropriate
+          interventions and treatments.
+        </p>
+      </div>
+    </section>
+  );
 }
 
-class PeopleSummary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      default: {
-        bioLaurent: {
-          name: 'Dr. Laurent Couetil',
-          image: picLaurent,
-          qualifications: 'DVM, PhD, Diplomate ACVIM-LAIM',
-          content:
-            'Dr. Couetil is a Professor of Large Animal Medicine at Purdue University, Director of the Equine Sports Medicine Center, and Director of Equine Research. A former practicing equine veterinarian, he has published extensively on respiratory diseases.',
-          link: 'https://vet.purdue.edu/directory/person.php?id=12',
-        },
-        bioKathleen: {
-          name: 'Dr. Kathleen Ivester',
-          image: picKathleen,
-          qualifications: 'DVM, PhD, Diplomate ACVS',
-          content:
-            'Dr. Ivester is an equine research scientist at Purdue University. A boarded equine surgeon, she bridges research and practice with field research investigating the links between environmental factors, viruses, and bacteria to equine respiratory health and performance.',
-          link: 'https://www.vet.purdue.edu/directory/person.php?id=279',
-          rtl: true,
-        },
-      },
-    };
-  }
-
-  render() {
-    return (
-      <section id="people-summary">
-        <h2>The Experts</h2>
-        <div className="bio-showcase">
-          <Bio {...this.state.default.bioLaurent} />
-          <Bio {...this.state.default.bioKathleen} />
-        </div>
-      </section>
-    );
-  }
+function PeopleSummary () {
+  return (
+    <section id="people-summary">
+      <h2>The Experts</h2>
+      <div className="bio-showcase">
+        <Bio
+          name="Dr. Laurent Couetil"
+          image={picLaurent}
+          qualifications="DVM, PhD, Diplomate ACVIM-LAIM"
+          link="https://vet.purdue.edu/directory/person.php?id=12"
+        >
+          Dr. Couetil is a Professor of Large Animal Medicine at Purdue
+          University, Director of the Equine Sports Medicine Center, and
+          Director of Equine Research. A former practicing equine
+          veterinarian, he has published extensively on respiratory diseases.
+        </Bio>
+        <Bio
+          name="Dr. Kathleen Ivester"
+          image={picKathleen}
+          qualifications="DVM, PhD, Diplomate ACVS"
+          link="https://www.vet.purdue.edu/directory/person.php?id=279"
+          rtl
+        >
+          Dr. Ivester is an equine research scientist at Purdue University. A
+          boarded equine surgeon, she bridges research and practice with
+          field research investigating the links between environmental
+          factors, viruses, and bacteria to equine respiratory health and
+          performance.
+        </Bio>
+      </div>
+    </section>
+  );
 }
 
-class Bio extends Component {
-  constructor(props) {
-    super(props);
-    const { rtl, ...rest } = props;
-    this.state = {
-      classes: `bio${rtl ? ' bio-rtl' : ' bio-ltr'}`,
-      ...rest,
-    };
-  }
-
-  render() {
-    return (
-      <div className={this.state.classes}>
-        <img src={this.state.image} alt="One of our Equine Experts" />
+function Bio ({ rtl, image, name, qualifications, children, link }) {
+  return (
+    <div className={`bio ${rtl ? 'bio-rtl' : 'bio-ltr'}`}>
+      <img src={image} alt={`Equine Expert ${name}`} />
         <div>
-          <h1>{this.state.name}</h1>
-          <h2>{this.state.qualifications}</h2>
-          <p>{this.state.content}</p>
-          <a href={this.state.link}>(see full bio)</a>
+          <h1>{name}</h1>
+          <h2>{qualifications}</h2>
+          <p>{children}</p>
+          <Link href={link}>(see full bio)</Link>
         </div>
-      </div>
-    );
-  }
-}
-
-class ServiceItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...props };
-  }
-
-  render() {
-    return (
-      <div className="summary-item">
-        <h3>{this.state.title}</h3>
-        <LinkAction {...this.state.action} />
-        <p>{this.state.content}</p>
-      </div>
-    );
-  }
-}
-
-const LinkAction = (props) => (
-  <a className="link-action" href={props.link}>
-    <span>{props.text}</span>
-  </a>
-);
-
-class Banner extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...props };
-  }
-
-  render() {
-    return (
-      <section id="home-banner" className="banner">
-        <div id="home-banner-message" className="banner-message">
-          <h1>
-            <span className="title-a">{this.state.titleA}</span>
-            <span className="title-b">{this.state.titleB}</span>
-          </h1>
-          <p>{this.state.content}</p>
-          <a href={this.state.action.link}>{this.state.action.text}</a>
-        </div>
-      </section>
-    );
-  }
+    </div>
+  );
 }
